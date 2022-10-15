5b9ce45e5120db7758478eb5e98c4afa8f2da2bd
Ashley Tyagi <ashleytyagi@hotamil.com>
2022-10-15 21:02:46 +0100

    Added migrations updated tests

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Thing
+from django.core.exceptions import ValidationError
 # Create your tests here.
+class thingsTest(TestCase):
+    def setUp(self):
+        self.user = Thing.objects.create(
+            name= 'Jane',
+            description= 'fjsdfklsdjflsdjf',
+            quantity=2,
+        )
+
+    def test_valid_input_data(self):
+        try:
+            self.user.full_clean()
+        except ValidationError:
+            self.fail("Test user not valid")
+
+
+
